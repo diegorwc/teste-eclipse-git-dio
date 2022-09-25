a9e35946195702155f8b1f8cfe2807c368a2d3b1
diegorwc <wille.diegoricardo@gmail.com>
2022-09-25 10:42:24 -0300

    Primeiro commit

---
+++ HelloWorldDio/src/com/dio/Hello.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.newJ.Carro;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		System.out.println("Hello World!" + (a + b));
+		Carro carro = new Carro();
+	}
+ 
+}
---
+++ HelloWorldDio/src/com/dio/newJ/Carro.java
@@ -0,0 +1,21 @@
+package com.dio.newJ;
+
+public class Carro {
+
+	private String marca;
+	private String modelo;
+	private String cor;
+	private Integer capacidade; // Número de passageiros
+
+	public Carro(String marca, String modelo, String cor, Integer capacidade) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.cor = cor;
+		this.capacidade = capacidade;
+	}
+	
+	public Carro() {
+		
+	}
+
+}
